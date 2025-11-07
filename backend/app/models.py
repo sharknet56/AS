9ce129e3f70bea235606d3c6155cdf1beebf2a67
Zhiteng Li <zhiteng.li2003@gmail.com>
2025-11-07 22:54:52 +0100

    make the password nullable so we can login using google and not password

--- backend/app/models.py
+++ backend/app/models.py
@@ -8,7 +8,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    hashed_password = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
 
     images = relationship("Image", back_populates="owner", cascade="all, delete-orphan")
